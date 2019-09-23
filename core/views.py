af9c747d5e8f719452ec8bd058e22b1791e2e0dc
JinHyeok Park <jhp@example.com>
2019-09-24 01:31:57 +0900

    modified views.py and add requirements.txt to .gitignore

--- core/views.py
+++ core/views.py
@@ -21,3 +21,15 @@ def example(request):
     return render(request, 'core/example.html')
 
 
+def beanjari(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if email == '' or re.match(EMAIL_REGEX, email) is None:
+            messages.error(request, "이메일 형식을 다시 확인해주세요😂")
+            return HttpResponseRedirect(request.path)
+
+        Email.objects.create(email=request.POST['email'], submitted_from=request.path)
+        messages.success(request, "이메일이 성공적으로 저장되었어요!😁")
+        return HttpResponseRedirect(request.path)
+
+    return render(request, 'core/beanjari/beanjari.html')
