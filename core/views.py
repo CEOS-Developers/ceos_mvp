c1c42975a3d1e9e835359e5a2df645347e2c7b14
gywls517 <gywls517@naver.com>
2019-09-24 11:42:02 +0900

    team2 fitster page completed

--- core/views.py
+++ core/views.py
@@ -7,17 +7,33 @@ import re
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 
-def example(request):
+def fitster(request):
     if request.method == 'POST':
         email = request.POST['email']
         if email == '' or re.match(EMAIL_REGEX, email) is None:
-            messages.error(request, "뭔가 잘못됨! 확인해라, 이메일!")
+            messages.error(request, "에러!")
             return HttpResponseRedirect(request.path)
 
         Email.objects.create(email=request.POST['email'], submitted_from=request.path)
-        messages.success(request, "성공적으로 저장!")
-        return HttpResponseRedirect(request.path)
+        if email == 'heart@gmail.com':
+            messages.success(request, "감사합니다!")
+        if email == 'more@gmail.com' or email == 'heart@gmail.com':
+            return render(request, 'core/fitsterL.html')
+        
+    return render(request, 'core/fitster.html')
 
-    return render(request, 'core/example.html')
+def ffitster(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if email == '' or re.match(EMAIL_REGEX, email) is None:
+            messages.error(request, "에러!")
+            return HttpResponseRedirect(request.path)
 
+        Email.objects.create(email=request.POST['email'], submitted_from=request.path)
+        if email == 'fheart@gmail.com':
+            messages.success(request, "감사합니다!")
+        if email == 'fmore@gmail.com' or email == 'fheart@gmail.com':
+            return render(request, 'core/ffitsterL.html')
+        
+    return render(request, 'core/ffitster.html')
 
