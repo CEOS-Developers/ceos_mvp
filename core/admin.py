526334f080eb7664827182d9375b5b7fe56351ce
hanqyu <peakjnut37@gmail.com>
2019-09-25 01:13:52 +0900

    add list_display for EmailAdmin

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from core.models import Email
 
 
-admin.site.register(Email)
+@admin.register(Email)
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ('email', 'submitted_from', 'created_at')
