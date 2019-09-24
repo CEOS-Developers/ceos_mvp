829e5a8f704ac7ad7706001c17e8a0a703495311
gywls517 <47051596+gywls517@users.noreply.github.com>
2019-09-24 13:45:35 +0900

    team1 fitster page completed (#5)
    * team1 fitster page completed
    
    * reflect modification
    
    * reflect modification2

--- core/views.py
+++ core/views.py
@@ -6,7 +6,6 @@ import re
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
-
 def example(request):
     if request.method == 'POST':
         email = request.POST['email']
@@ -20,6 +19,35 @@ def example(request):
 
     return render(request, 'core/example.html')
 
+def fitster(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if email == '' or re.match(EMAIL_REGEX, email) is None:
+            messages.error(request, "에러!")
+            return HttpResponseRedirect(request.path)
+
+        Email.objects.create(email=request.POST['email'], submitted_from=request.path)
+        if email == 'heart@gmail.com':
+            messages.success(request, "감사합니다!")
+        if email == 'more@gmail.com' or email == 'heart@gmail.com':
+            return render(request, 'core/fitsterL.html')
+        
+    return render(request, 'core/fitster.html')
+
+def ffitster(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if email == '' or re.match(EMAIL_REGEX, email) is None:
+            messages.error(request, "에러!")
+            return HttpResponseRedirect(request.path)
+
+        Email.objects.create(email=request.POST['email'], submitted_from=request.path)
+        if email == 'fheart@gmail.com':
+            messages.success(request, "감사합니다!")
+        if email == 'fmore@gmail.com' or email == 'fheart@gmail.com':
+            return render(request, 'core/ffitsterL.html')
+        
+    return render(request, 'core/ffitster.html')
 
 def beanjari(request):
     if request.method == 'POST':
